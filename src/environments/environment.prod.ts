0a43ab921cc43c80477c765794d27b4fd592becc
Martin <machacek-martin@seznam.cz>
2018-08-02 23:12:43 +0200

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBY-9tTZR8iUlkRyXaRBke7vQH4JTYZdWw",
+    authDomain: "cosmonauts-b3080.firebaseapp.com",
+    databaseURL: "https://cosmonauts-b3080.firebaseio.com",
+    projectId: "cosmonauts-b3080",
+    storageBucket: "cosmonauts-b3080.appspot.com",
+    messagingSenderId: "64266436620"
+  }
 };
